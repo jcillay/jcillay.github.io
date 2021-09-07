3a109e1042079b360760a7b75d4b64bdd5426b91
jcillay <jcillay.5@gmail.com>
2021-09-07 14:02:07 -0700

    Updated

--- myWebsite/main.js
+++ myWebsite/main.js
@@ -1,6 +1,6 @@
 // import './style.css' ;
-// import * as THREE from 'three';
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from 'three/build/three.module';
+// import * as THREE from 'https://cdn.skypack.dev/three@<version>';
 
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
