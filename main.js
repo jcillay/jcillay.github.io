c6fb0153812c890b6e2d2d77d0a4dcae2c7456a7
jcillay <jcillay.5@gmail.com>
2021-09-08 23:17:07 -0700

    Added photos, some js, updated words in index file and made the read less better

---
+++ main.js
@@ -0,0 +1,29 @@
+function showMore() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readMoreButton");
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read More";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read Less";
+      moreText.style.display = "inline";
+    }
+  }
+
+//   function showMoreSpotify() {
+//     var dots = document.getElementById("dots");
+//     var moreText = document.getElementById("more");
+//     var btnText = document.getElementById("readMoreButton");
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Read More";
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Read Less";
+//       moreText.style.display = "inline";
+//     }
+//   }
